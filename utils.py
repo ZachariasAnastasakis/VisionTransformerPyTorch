d6c50e999695042a029d5c863e3f0d1bf7b8bdf1
ZachariasAnastasakis <zaxarisanastasakis@gmail.com>
2023-06-05 21:59:57 +0300

    added comments

--- utils.py
+++ utils.py
@@ -5,6 +5,20 @@ import torch.nn as nn
 class Img2PatchEmbed(nn.Module):
     def __init__(self, in_channels, img_size, emb_dim, patch_size):
         super(Img2PatchEmbed, self).__init__()
+        """
+        Class to divide an image to patches.
+        
+        Arguments:
+        --------------
+        in_channels: int, the number of channels of the image
+        img_size: int, size of the image (width = height)
+        emb_dim: int, the number of dimensions of the embedding
+        patch_size: int, size of the patch (patch_width = patch_height)
+        
+        Returns:
+        -------------
+        x: Tensor (N, num_patches, emb_dim), the sequence of patches
+        """
 
         self.num_patches = (img_size // patch_size) ** 2
         self.patch_size = patch_size
@@ -14,28 +28,28 @@ class Img2PatchEmbed(nn.Module):
                                     stride=patch_size)
 
     def forward(self, x):
-        x = self.projection(x)  # (batch_size, emb_dim, num_patches ** 0.5, num_patches ** 0.5)
-        x = x.flatten(2)  # (batch_size, emb_dim, num_patches)
-        x = x.transpose(1, 2)  # (batch_size, num_patches, emb_dim)
+        x = self.projection(x)  # (N, emb_dim, num_patches ** 0.5, num_patches ** 0.5)
+        x = x.flatten(2)  # (N, emb_dim, num_patches)
+        x = x.transpose(1, 2)  # (N, num_patches, emb_dim)
 
         return x
-
-
-# class LearnablePositionalEmbedding(nn.Module):
-#     def __int__(self, num_patches, emb_dim):
-#         super(LearnablePositionalEmbedding, self).__init__()
-#
-#         self.num_patches = num_patches
-#         self.emb_dim = emb_dim
-#         self.pos_emb = nn.Parameter(torch.randn(self.num_patches, emb_dim), requires_grad=True)
-#
-#     def forward(self):
-#         return self.pos_emb
 
 
 class MultiHeadAttention(nn.Module):
     def __init__(self, emb_dim, num_heads):
         super(MultiHeadAttention, self).__init__()
+        """
+        Class for computing multihead attention.
+        
+        Arguments
+        ---------
+        emb_dim: int, the number of dimensions of the embedding
+        num_heads: int, the number of MSA heads
+        
+        Returns
+        -------
+        multihead_attention: Tensor (N, num_patches, emb_dim), the calculated multihead attention 
+        """
 
         self.emb_idm = emb_dim
         self.num_heads = num_heads
@@ -52,23 +66,23 @@ class MultiHeadAttention(nn.Module):
         self.head_projection = nn.Linear(self.num_heads * self.head_dim, self.emb_idm)
 
     def forward(self, x):
-        # x -> (batch_size, num_patches, emb_dim)
-        batch_size, num_patches, _ = x.shape
+        # x: (N, num_patches, emb_dim)
+        N, num_patches, _ = x.shape
         query, key, value = x, x, x
 
-        q_x = query.reshape(batch_size, num_patches, self.num_heads, self.head_dim)
-        k_x = key.reshape(batch_size, num_patches, self.num_heads, self.head_dim)
-        v_x = value.reshape(batch_size, num_patches, self.num_heads, self.head_dim)
+        q_x = query.reshape(N, num_patches, self.num_heads, self.head_dim)  # (N, num_patches, num_heads, head_dim)
+        k_x = key.reshape(N, num_patches, self.num_heads, self.head_dim)  # (N, num_patches, num_heads, head_dim)
+        v_x = value.reshape(N, num_patches, self.num_heads, self.head_dim)  # (N, num_patches, num_heads, head_dim)
 
         q_x = self.query(q_x)
         k_x = self.key(k_x)
         v_x = self.value(v_x)
 
-        dot_product = torch.matmul(q_x, k_x.transpose(-1, -2))
+        dot_product = torch.matmul(q_x, k_x.transpose(-1, -2))  # (N, num_patches, num_heads, num_heads)
         attention = nn.Softmax(-1)(dot_product * self.scale)
 
-        scaled_dot_product_attention_per_head = torch.matmul(attention, v_x)
-        scaled_dot_product_attention = scaled_dot_product_attention_per_head.flatten(2)
+        scaled_dot_product_attention_per_head = torch.matmul(attention, v_x)  # (N, num_patches, num_heads, head_dim)
+        scaled_dot_product_attention = scaled_dot_product_attention_per_head.flatten(2)  # (N, num_patches, emb_dim)
 
         multihead_attention = self.head_projection(scaled_dot_product_attention)
 
@@ -78,6 +92,19 @@ class MultiHeadAttention(nn.Module):
 class MLP(nn.Module):
     def __init__(self, emb_dim, mlp_ratio):
         super(MLP, self).__init__()
+        """
+        Class to implement the MLP of the MSA module
+        
+        Arguments
+        ---------
+        emb_dim: int, the number of dimensions of the embedding
+        mlp_ratio: int, configures the hidden dimension of the mlp (mlp_ratio * emb_dim)
+        
+        Returns
+        -------
+        Tensor of shape (N, num_patches, emb_dim)
+        """
+
         self.mlp = nn.Sequential(
             nn.Linear(emb_dim, mlp_ratio * emb_dim), nn.GELU(),
             nn.Linear(emb_dim * mlp_ratio, emb_dim)
@@ -90,6 +117,19 @@ class MLP(nn.Module):
 class ViTBlock(nn.Module):
     def __init__(self, embed_dim, num_heads, mlp_ratio):
         super(ViTBlock, self).__init__()
+        """
+        Class to implement the ViT Block.
+        
+        Arguments
+        ---------
+        emb_dim: int, the number of dimensions of the embedding
+        mlp_ratio: int, configures the hidden dimension of the mlp (mlp_ratio * emb_dim)
+        num_heads: int, the number of MSA heads
+        
+        Returns
+        -------
+        Tensor (N, num_patches, emb_dim), the output of a ViT block
+        """
 
         self.multihead_attention = MultiHeadAttention(embed_dim, num_heads)
         self.mlp = MLP(embed_dim, mlp_ratio=mlp_ratio)
--- vit.py
+++ vit.py
@@ -6,6 +6,23 @@ from utils import Img2PatchEmbed, ViTBlock
 class ViT(nn.Module):
     def __init__(self, in_channels, img_size, emb_dim, num_heads, patch_size, depth, mlp_ratio, num_classes):
         super(ViT, self).__init__()
+        """
+        Class that performs a simple implementation of the Vision Transformer.
+        
+        Arguments
+        ---------
+        in_channels: int, the number of channels of the image
+        img_size: int, size of the image (width = height)
+        emb_dim: int, the number of dimensions of the embedding
+        num_heads: int, the number of MSA heads
+        patch_size: int, size of the patch (patch_width = patch_height)
+        depth: int, number of blocks
+        mlp_ratio: int, configures the hidden dimension of the mlp (mlp_ratio * emb_dim)
+        num_classes: int, number of classes for the classification problem
+        
+        Returns
+        scores: Tensor (N, num_classes), the classification scores
+        """
 
         self.img_size = img_size
         self.num_patches = (img_size // patch_size) ** 2
@@ -22,17 +39,15 @@ class ViT(nn.Module):
         ])
 
     def forward(self, x):
-        N, _, _, _ = x.shape
+        N, _, _, _ = x.shape  # (N, in_channels, img_size, img_size)
         x = self.img_emb(x)  # (N, num_patches, emb_dim)
-        cls_token = self.cls_token.expand(
-            N, -1, -1
-        )  # (N, 1, emb_dim)
-        x = torch.cat((cls_token, x), dim=1)
-        x = x + self.pos_emb
+        cls_token = self.cls_token.expand(N, -1, -1)  # (N, 1, emb_dim)
+        x = torch.cat((cls_token, x), dim=1)  # (N, num_patches+1, emb_dim)
+        x = x + self.pos_emb  # (N, num_patches+1, emb_dim)
         for vit_block in self.vit_blocks:
             x = vit_block(x)
 
-        cls_token = x[:, 0]
-        scores = self.classifier(cls_token)
+        cls_token = x[:, 0]  # (N, emb_dim)
+        scores = self.classifier(cls_token)  # (N, num_classes)
 
         return scores
